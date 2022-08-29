ef1a70ddeb4827c9160be1b6357d91da5d03fe51
flowerinsnow <flowerinsnow@donghua.email.cn>
2022-08-29 14:25:15 +0100

    修复数据库建表的一处语法错误

--- src/main/kotlin/online/flowerinsnow/flowerairadar/kook/database/DatabaseTables.kt
+++ src/main/kotlin/online/flowerinsnow/flowerairadar/kook/database/DatabaseTables.kt
@@ -7,7 +7,7 @@ enum class DatabaseTables(val table : SQLTable) {
     PERMISSION(
         SQLTable.of("permission",
         arrayOf("`id` INT PRIMARY KEY AUTO_INCREMENT",
-        "`user` VARCHAR(16) NOT NULL,",
+        "`user` VARCHAR(16) NOT NULL",
         "`permission` VARCHAR(255) NOT NULL",
         "`scope` VARCHAR(16) NOT NULL",
         "INDEX(`user`, `permission`, `scope`)"))
--- src/main/kotlin/online/flowerinsnow/flowerairadar/kook/listener/TestListener.kt
+++ src/main/kotlin/online/flowerinsnow/flowerairadar/kook/listener/TestListener.kt
@@ -1,9 +1,7 @@
 package online.flowerinsnow.flowerairadar.kook.listener
 
 import love.forte.di.annotation.Beans
-import love.forte.simboot.annotation.Filter
 import love.forte.simboot.annotation.Listener
-import love.forte.simboot.filter.MatchType
 import love.forte.simbot.LoggerFactory
 import love.forte.simbot.event.ChannelMessageEvent
 
@@ -11,8 +9,9 @@ import love.forte.simbot.event.ChannelMessageEvent
 class TestListener {
     private val logger = LoggerFactory.getLogger<TestListener>()
     @Listener
-    @Filter("/myid", matchType = MatchType.TEXT_EQUALS)
     suspend fun ChannelMessageEvent.testListener() {
-        reply("您的ID是 ${author().id}")
+        if (messageContent.plainText == "/myid") {
+            reply("您的ID是 ${author().id}")
+        }
     }
 }
